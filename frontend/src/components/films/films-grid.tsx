b53e170a7906e61e562f9b0493e748bf9f9e8961
Bhushan Ahuja <bhushanahujawork@gmail.com>
2026-09-08 18:28:28 +0530

    Restore red hover on film arrow button

--- frontend/src/components/films/films-grid.tsx
+++ frontend/src/components/films/films-grid.tsx
@@ -136,9 +136,9 @@ function FilmCard({
         </div>
       </div>
 
-      <span className="absolute -right-2 -bottom-2 w-12 h-12 md:-right-3 md:w-[64px] md:h-[64px] rounded-full bg-[#faf8f4] shadow-lg flex items-center justify-center transition-all duration-300 group-hover:bg-[#d4af37] group-hover:shadow-xl group-hover:shadow-black/20">
+      <span className="absolute -right-2 -bottom-2 w-12 h-12 md:-right-3 md:w-[64px] md:h-[64px] rounded-full bg-[#faf8f4] shadow-lg flex items-center justify-center transition-all duration-300 group-hover:bg-[#FF0000] group-hover:shadow-xl group-hover:shadow-red-500/50">
         <svg
-          className="w-5 h-5 md:w-6 md:h-6 text-[#1f2428] transition-colors duration-300 group-hover:text-black"
+          className="w-5 h-5 md:w-6 md:h-6 text-[#1f2428] transition-colors duration-300 group-hover:text-white"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
